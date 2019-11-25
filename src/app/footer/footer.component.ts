5a2a74e5ece5667f2103f9e07326c742db250827
hasburg <hasburg12@gmail.com>
2019-11-25 17:31:49 +0200

    footer lang

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { eng } from "../../environments/lang/eng.js";
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  source = eng;
   constructor() { }
 
   ngOnInit() {
