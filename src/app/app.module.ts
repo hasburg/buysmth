1afbdd073c8aaaf769e62b08aa671e1d1112194a
hasburg <hasburg12@gmail.com>
2019-11-26 02:38:20 +0200

    add pick

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ScrollerComponent } from './scroller/scroller.component';
 import { FaqComponent } from './faq/faq.component';
 import { FooterComponent } from './footer/footer.component';
+import { PhoneComponent } from './phone/phone.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ScrollerComponent,
       FaqComponent,
-      FooterComponent
+      FooterComponent,
+      PhoneComponent
    ],
    imports: [
       BrowserModule,
